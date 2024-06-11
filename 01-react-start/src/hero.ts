2adbed491ab419242131157273444e557df872a5
Diego Mejia <diego.mejia@rappi.com>
2024-06-10 21:32:54 -0500

    feat: Created Hero data

---
+++ 01-react-start/src/hero.ts
@@ -0,0 +1,19 @@
+export interface IHero {
+  id: number;
+  name: string;
+}
+
+export const heroes: IHero[] = [
+  {
+    name: 'Batman',
+    id: 1,
+  },
+  {
+    name: 'Superman',
+    id: 2,
+  },
+  {
+    name: 'The Flash',
+    id: 3,
+  },
+];
